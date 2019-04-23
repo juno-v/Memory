74b462561b7da6209a89c74e58bef65c8a6d9ec7
Juno Vue <Juno.Vue@Hotmail.com>
2019-04-23 14:30:28 -0500

    able to post ENTRIES and IMAGES table in one post route using client.query in entries.router.js

--- server/routes/entries.router.js
+++ server/routes/entries.router.js
@@ -2,41 +2,31 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-// add user's journal entry 
-router.post('/', (req, res) => {
-    console.log(`hit entries.router POST`);
-    
-    const newEntry = req.body;
-    const entryText = `INSERT INTO "entries" ("user_id", "title", "date", "description", "location")
-                       VALUES ($1, $2, $3, $4, $5) RETURNING id;`
-    const entryValues = [
+router.post('/', async(req, res, next) =>{
+  const client = await pool.connect();
+  const newEntry = req.body;
+  try {
+    await client.query('BEGIN')
+    const entry = await client.query(`INSERT INTO "entries" ("user_id", "title", "date", "description", "location")
+    VALUES ($1, $2, $3, $4, $5) RETURNING id;`, [
       newEntry.user_id,
       newEntry.title,
       newEntry.date,
       newEntry.description,
       newEntry.location,
-    ];
-    const imageText = `INSERT INTO "images" ("file") VALUES($1);`
+    ])
+    console.log(entry.rows[0].id)
+    const insertPhotoText = `INSERT INTO "images" ("file", "entries_id") VALUES($1,$2);`
+    const insertPhotoValues = [newEntry.imageValues, entry.rows[0].id]
+    await client.query(insertPhotoText, insertPhotoValues)
+    await client.query('COMMIT')
+  }catch (e) {
+    await client.query('ROLLBACK')
+    throw e
+  } finally {
+    client.release()
+  }
+})
 
-    const imageValues = [
-      newEntry.file
-    ];
-    pool.query(entryText, entryValues)
-    pool.query(imageText, imageValues)
-      .then(() => { res.sendStatus(201); })
-      .catch((err) => {
-        console.log('Error POSTING journal entries into the database', err);
-        res.sendStatus(500);
-      });
-  });
 
 module.exports = router;
-
-/* 
-const imageText = `INSERT INTO "images" ("file")
-VALUES($1);`
-
-const imageValues = [
-  newEntry.file
-];
-*/
