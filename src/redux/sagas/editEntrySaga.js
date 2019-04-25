7c04fba493e0dea04b1da8f52fcd86d378b5d743
Juno Vue <Juno.Vue@Hotmail.com>
2019-04-25 16:15:20 -0500

    fixed minor bug, couldn't get to PUT route because of a const definition for a console.log

--- src/redux/sagas/editEntrySaga.js
+++ src/redux/sagas/editEntrySaga.js
@@ -5,12 +5,11 @@ function* editEntries(action) {
     try {
     console.log(`HIT editEntries`);
     console.log(`action.payload is: `, action.payload);
-    console.log(`user.id`, action.payload.id );
-    const id = action.payload.id
+    const id = action.payload.entryId
     
-    
-     const response = yield axios.put(`/entry/edit/${id}`, action.payload);
-    yield put({ type: 'GET_ENTRIES' , payload: response.data })
+    const response = yield axios.put(`/entry/edit/${id}`, action.payload);
+    console.log(`response.data is: `, response.data)
+    yield put({ type: 'GET_ENTRIES', payload: response.data  })
     }
     catch (error) {
       console.log(`Couldn't get user's entries`);
