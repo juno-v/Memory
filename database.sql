91d22960db9d11d8b5da5ce67ba35bd079cc9aed
Juno Vue <Juno.Vue@Hotmail.com>
2019-06-07 09:53:44 -0500

    adjusted database.sql file to include all tables for the project

--- database.sql
+++ database.sql
@@ -1,10 +1,22 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+
+CREATE TABLE "entries" (
+	"id" SERIAL PRIMARY KEY, 
+	"user_id" INT ,
+	"title" varchar(80), 
+	"date" date , 
+	"description" varchar ,
+	"location" varchar,
+	"url" varchar
+);
+
+CREATE TABLE "images" (
+	"id" SERIAL PRIMARY KEY, 
+	"entries_id" int,
+	"file" varchar
 );
